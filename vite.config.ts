e97056dc4ae39417de69a837e6e90ffbee418fc3
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2025-12-15 14:34:16 +0700

    edit base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/pemilu-app/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
